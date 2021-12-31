b733e8e10943ec192615ba9c8769ad72afe36c81
pongpatapee <pongpatapee1021@gmail.com>
2021-12-31 00:10:14 -0800

    made UI for rps game

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let choice = ["rock", "paper", "sissor"];
-let winning = {"rock": "sissor", "paper":"rock", "sissor":"paper"};
+let choice = ["rock", "paper", "scissor"];
+let winning = {"rock": "scissor", "paper":"rock", "scissor":"paper"};
 let score = {"player": 0, "computer": 0};
 
 function computerPlay() {
@@ -23,17 +23,63 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function capitalize(str){
+    let firstLetter = str[0].toUpperCase();
+    return firstLetter + str.slice(1);
+}
+
 function game() {
-    let playerSelection = prompt("Input your move: ");
+    // let playerSelection = prompt("Input your move: ");
+
+    const playerChoiceDisplay = document.querySelector('.player-choice');
+    const computerChoiceDisplay = document.querySelector('.computer-choice');
+    const playerScoreDisplay = document.querySelector('.player-score');
+    const computerScoreDisplay = document.querySelector('.computer-score');
+    const displayMessage = document.querySelector('.message');
+
+    const playerBtns = document.querySelectorAll('.player-btn');
+    playerBtns.forEach(btn => btn.addEventListener('click', (e)=>{
+        let playerSelection = btn.id;
+        let computerSelection = computerPlay();
+
+        playerChoiceDisplay.innerText = capitalize(playerSelection);
+        computerChoiceDisplay.innerText = capitalize(computerSelection);
+
+        let message = playRound(playerSelection, computerSelection);
+
+        displayMessage.innerText = message;
+        playerScoreDisplay.innerText = `Player: ${score["player"]}`
+        computerScoreDisplay.innerText = `Computer: ${score["computer"]}`
+
+        console.log(`Player: ${playerSelection} Computer: ${computerSelection}`)
+        console.log(message);
+        console.log(`Player=${score["player"]} Computer=${score["computer"]}`);
+    }));
+
+}
+
+// window.addEventListener('click', game);
+
+const playerBtns = document.querySelectorAll('.player-btn');
+playerBtns.forEach(btn => btn.addEventListener('click', (e)=>{
+    const playerChoiceDisplay = document.querySelector('.player-choice');
+    const computerChoiceDisplay = document.querySelector('.computer-choice');
+    const playerScoreDisplay = document.querySelector('.player-score');
+    const computerScoreDisplay = document.querySelector('.computer-score');
+    const displayMessage = document.querySelector('.message');
+    let playerSelection = btn.id;
     let computerSelection = computerPlay();
-    console.log(`Player: ${playerSelection}`);
-    console.log(`Computer: ${computerSelection}`);
-    console.log(playRound(playerSelection, computerSelection));
-    console.log(`Score:`);
+
+    playerChoiceDisplay.innerText = capitalize(playerSelection);
+    computerChoiceDisplay.innerText = capitalize(computerSelection);
+
+    let message = playRound(playerSelection, computerSelection);
+
+    displayMessage.innerText = message;
+    playerScoreDisplay.innerText = `Player: ${score["player"]}`
+    computerScoreDisplay.innerText = `Computer: ${score["computer"]}`
+
+    console.log(`Player: ${playerSelection} Computer: ${computerSelection}`)
+    console.log(message);
     console.log(`Player=${score["player"]} Computer=${score["computer"]}`);
-}
-
-for(let i = 0; i < 5; i++){
-    game();
-}
-
+}));
